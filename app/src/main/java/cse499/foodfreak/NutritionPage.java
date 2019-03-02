8f551c000b5cc615aefbc85c3a3ed793fe4d4077
Shanto16 <shakil.glostars@gmail.com>
2019-03-03 01:28:16 +0600

    different activity for different pages

---
+++ app/src/main/java/cse499/foodfreak/NutritionPage.java
@@ -0,0 +1,13 @@
+package cse499.foodfreak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NutritionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_page);
+    }
+}
---
+++ app/src/main/java/cse499/foodfreak/StartingPage.java
@@ -0,0 +1,13 @@
+package cse499.foodfreak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_page);
+    }
+}
