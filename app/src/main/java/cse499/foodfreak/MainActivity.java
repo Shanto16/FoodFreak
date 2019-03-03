ac40d59dc5cd560bc333e5dc065722c8fa9179a8
Zozonee <sabihazozonee@gmail.com>
2019-03-04 00:36:36 +0600

    camera pop up

--- app/src/main/java/cse499/foodfreak/MainActivity.java
+++ app/src/main/java/cse499/foodfreak/MainActivity.java
@@ -1,13 +1,31 @@
 package cse499.foodfreak;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cameraCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraCard = findViewById(R.id.camera_card);
+
+
+        cameraCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+
+            }
+        });
     }
 }
